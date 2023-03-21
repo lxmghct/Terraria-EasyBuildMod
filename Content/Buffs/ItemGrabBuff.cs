7deb706f622fac13b539c62473c67977623bad8e
longxiaoming <1781092881@qq.com>
2023-03-22 00:16:32 +0800

    Fix: 修复Buff栏名称显示bug

--- Content/Buffs/ItemGrabBuff.cs
+++ Content/Buffs/ItemGrabBuff.cs
@@ -4,15 +4,9 @@ namespace EasyBuildMod.Content.Buffs
 {
     public class ItemGrabBuff : ModBuff
     {
-        internal static string GetText(string str, params object[] args)
-        {
-            return Language.GetTextValue($"Mods.EasyBuildMod.Content.Buffs.ItemGrabBuff.{str}", args);
-        }
         
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault(GetText("Name"));
-            Description.SetDefault(GetText("Description"));
             Main.buffNoTimeDisplay[Type] = true;
             Main.debuff[Type] = false;
         }
